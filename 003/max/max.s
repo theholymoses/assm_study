8701fa72a6fc7a40387bdbccc4b8cf5bb284bc03
moses <shroomce@gmail.com>
2022-12-18 18:34:41 +0500

    Added comments to max program

--- 003/max/max.s
+++ 003/max/max.s
@@ -1,4 +1,4 @@
-# PURPOSE:
+#	PURPOSE:
 #		Find the maximum number in a set of data items.
 #
 #	VARIABLES:
@@ -14,28 +14,38 @@ data_items:
 	.long	1,2,3,243,5,6,7,8,9,10,0
 
 .section .text
-
 .globl _start
+
 _start:
-	movl $0, %edi											# start reading from 0 index
-	movl data_items(,%edi,4), %eax		#	load the first byte of data to %eax
-	movl %eax, %ebx										# the first item is biggest
+	#	Load the first index into %edi
+	#	Load the first value from data_items
+	#	Since the first value is obviously the biggest - load it into %ebx register
+
+	movl $0, %edi
+	movl data_items(,%edi,4), %eax
+	movl %eax, %ebx
 
 start_loop:
-	cmpl $0, %eax											# check if we hit the end
+	#	1) Check if current value examined isn't zero. If it is zero - end the loop.
+	#	2) Increment the index by 1.
+	#	3) Move value from data_item by specified index into %eax, currently examined value.
+	#	4) If the new value in %eax isn't bigger than the current max value - jump to the start of the loop.
+	#	   Otherwise - load %eax value to %ebx.
+
+	cmpl $0, %eax
 		je loop_exit
 
-	incl %edi													# increment %edi value
-	movl data_items(,%edi,4), %eax		# load next value
-	cmpl %ebx, %eax										# compare values
-		jle start_loop									# jump to the beggining if the new values isn't bigger
+	incl %edi
+	movl data_items(,%edi,4), %eax
+	cmpl %ebx, %eax
+		jle start_loop
 
-	movl %eax, %ebx										# current value is bigger
+	movl %eax, %ebx
 	jmp start_loop
 
 loop_exit:
-	# status code will be a maximum number of the data_items
+	#	The max value will be used as a return status.
+
 	movl $1, %eax
 	int $0x80
-
 
